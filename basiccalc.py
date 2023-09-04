ae4638f1991ff44649867cc57f283b0ec3d3a4d3
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-04 22:05:14 +0700

    Add files via upload

---
+++ basiccalc.py
@@ -0,0 +1,11 @@
+"""
+	block comment
+	create a python program that would display the
+	sum of two()
+"""
+#this is a comment
+
+a=10
+b=20
+c=a+b
+print(f"the sum of %d and %d is %d" % (a,b,c))
---
+++ formatstr.py
@@ -0,0 +1,2 @@
+first = 'Kierzen'
+last = 'Booc'
