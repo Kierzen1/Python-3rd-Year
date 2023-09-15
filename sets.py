879f7ca5f65d1bd6f5bc89065eee545a952d7cd2
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-15 14:20:28 +0100

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,12 @@
+# create a set of integer type
+student_id = {112, 114, 116, 118, 115}
+student_id.add(123)#ADD
+student_id.discard(118)#REMOVE
+print('Student ID:', student_id)
+
+
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+vowel_letters.add('z')#add
+print('Vowel Letters:', vowel_letters)
+
