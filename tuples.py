bade18c701ebf34fdbd4c2e6b72bece1783bb98b
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-17 15:14:04 +0100

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# create a set of integer type
+student_id = (112, 114, 116, 118, 115)
+print(student_id[1])
+
+print('Student ID:', student_id)
+
+
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+vowel_letters.add('z')#add
+print('Vowel Letters:', vowel_letters)
+
