2661e8ac776724eddd23c7f65958d3a5c24ef29b
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-10-21 22:34:00 +0800

    Update dbhelper.py

--- GUI-ASSIGNMENT_FINAL/dbhelper.py
+++ GUI-ASSIGNMENT_FINAL/dbhelper.py
@@ -67,9 +67,12 @@ def deleterecord(table:str,**kwargs)->bool:
     sql:str = f"DELETE FROM `{table}` WHERE `{flds[0]}`='{flds[1]}'"
     return doProcess(sql)
 
+#print(getrecord("student",idno='0002'))
+#print(getall("student"))
+#updaterecord('student',idno='0004',lastname='foxtrot',firstname='gold',course='bscs',level='3')
+#print(deleterecord("student",idno='0002'))
 
 def get_all_records():
-    
     table = "student"
     db = db_connect()
     cursor = db.cursor(dictionary=True)
